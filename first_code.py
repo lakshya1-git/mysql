b561db3ac9f7a759ab8bfb836d34c8ccb93dfc16
lakshya1-git <lakshyapandey961@gmail.com>
2023-06-12 10:23:09 +0530

    firstcommit

--- first_code.py
+++ first_code.py
@@ -5,5 +5,6 @@ cursor = mydb.cursor()
 cursor.execute("create databaase lakshya") #create data base
 cursor.execute("show database")
 cursor.execute("create database")
-cursor.execute("create table lakshya.laksdetails()")
+cursor.execute("create table lakshya.laksdetails(employee_id in(10), employee_name varchar(80), employee_mail varchar(20), employee_sal int(6),employee_att int(2))")
+cursor.execute(s)
 print(cursor.fetchall())
